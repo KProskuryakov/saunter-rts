bdbec9c82e1d4ce58ab953d6e2494bc8ec1b3878
kproskuryakov <2399977+KProskuryakov@users.noreply.github.com>
2021-10-22 18:32:06 -0400

    Camera controls

--- src/Main.ts
+++ src/Main.ts
@@ -3,13 +3,13 @@ import Phaser from 'phaser';
 class Unit extends Phaser.Physics.Arcade.Sprite {
   movingToLoc?: Phaser.Math.Vector2;
   speed: number;
+  faction: string = 'player';
 
-  constructor(scene: Scene, x: number, y: number, texture: string, speed: number) {
+  constructor(scene: GameScene, x: number, y: number, texture: string, speed: number) {
     super(scene, x, y, texture);
     this.speed = speed;
     scene.add.existing(this);
     this.setInteractive();
-    this.scale = 2;
   }
 
   preUpdate(time: number, delta: number) {
@@ -22,12 +22,12 @@ class Unit extends Phaser.Physics.Arcade.Sprite {
   }
 }
 
-class Scene extends Phaser.Scene {
+class GameScene extends Phaser.Scene {
 
-  selected: Unit[] = [];
+  sprites: Phaser.GameObjects.Sprite[] = [];
 
   constructor() {
-    super('Scene');
+    super('GameScene');
   }
 
   preload() {
@@ -38,94 +38,111 @@ class Scene extends Phaser.Scene {
   create() {
     this.input.mouse.disableContextMenu();
 
-    this.add.text(20, 20, "Left-click to select, drag left button to select multiple.\nRight click to move command.\nEnter to fullscreen.");
+    this.cameras.main.setBounds(0, 0, 4096, 2160);
+    this.cameras.main.zoom = 1.0;
 
-    const spriteBounds = Phaser.Geom.Rectangle.Inflate(Phaser.Geom.Rectangle.Clone(this.physics.world.bounds), -20, -20);
-    const sprites: Phaser.GameObjects.Sprite[] = []
-    for (let i = 0; i < 5; i++) {
+    const uiScene = this.scene.get('UIScene') as UIScene;
+
+    const spriteBounds = Phaser.Geom.Rectangle.Inflate(Phaser.Geom.Rectangle.Clone(this.cameras.main.getBounds()), -20, -20);
+    for (let i = 0; i < 20; i++) {
       const pos = Phaser.Geom.Rectangle.Random(spriteBounds, new Phaser.Geom.Point());
-      const mushroom = this.physics.add.existing(new Unit(this, pos.x, pos.y, 'mushroom', 200));
-      sprites.push(mushroom);
+      const mushroom = this.physics.add.existing(new Unit(this, pos.x, pos.y, 'mushroom', 100));
+      this.sprites.push(mushroom);
     }
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 20; i++) {
       const pos = Phaser.Geom.Rectangle.Random(spriteBounds, new Phaser.Geom.Point());
-      const catgirl = this.physics.add.existing(new Unit(this, pos.x, pos.y, 'catgirl', 250));
-      sprites.push(catgirl);
+      const catgirl = this.physics.add.existing(new Unit(this, pos.x, pos.y, 'catgirl', 125));
+      this.sprites.push(catgirl);
     }
 
-    let dragbox = this.add.graphics();
-    let drawDragbox = false;
+    this.input.on('pointermove', function (this: GameScene, pointer: Phaser.Input.Pointer) {
+      if (pointer.middleButtonDown()) {
+        const cam = this.cameras.main;
+        cam.scrollX -= (pointer.x - pointer.prevPosition.x) / cam.zoom;
+        cam.scrollY -= (pointer.y - pointer.prevPosition.y) / cam.zoom;
+      }
+    }, this);
 
-    this.input.keyboard.on('keydown-ENTER', function (this: Scene) {
+    this.input.on('wheel', function (this: GameScene, pointer: Phaser.Input.Pointer) {
+      this.cameras.main.zoom = Phaser.Math.Clamp(this.cameras.main.zoom - .25 * Math.sign(pointer.deltaY), 0.5, 2.0)
+    });
+
+    this.input.on('pointerup', function (this: GameScene, pointer: Phaser.Input.Pointer, clickedObjects: Phaser.GameObjects.GameObject[]) {
+      if (pointer.leftButtonReleased()) {
+        this.sprites.forEach(sprite => {
+          sprite.setTint(0xffffff);
+        });
+        if (!uiScene.drawDragbox && clickedObjects.length > 0) {
+          uiScene.selected = [];
+          const unit = clickedObjects[0];
+          if (unit instanceof Unit) {
+            unit.setTint(0x00ff00);
+            uiScene.selected.push(unit);
+          }
+        } else {
+          uiScene.selected = [];
+          uiScene.drawDragbox = false;
+          uiScene.dragbox.clear();
+
+          const boxStart = this.cameras.main.getWorldPoint(Math.min(pointer.downX, pointer.x), Math.min(pointer.downY, pointer.y));
+          this.physics.overlapRect(boxStart.x, boxStart.y, pointer.getDistanceX() / this.cameras.main.zoom, pointer.getDistanceY() / this.cameras.main.zoom, true, false).forEach((body) => {
+            const obj = body.gameObject;
+            if (obj instanceof Unit) {
+              obj.setTint(0x00ff00);
+              uiScene.selected.push(obj);
+            }
+          });
+        }
+      } else if (pointer.rightButtonReleased()) {
+        uiScene.selected.forEach((unit) => {
+          this.physics.moveToObject(unit, pointer.positionToCamera(this.cameras.main), unit.speed);
+          unit.movingToLoc = pointer.positionToCamera(this.cameras.main) as Phaser.Math.Vector2;
+        });
+      }
+    }, this);
+
+    this.scene.launch('UIScene');
+
+  }
+
+  update() {
+  }
+
+}
+
+class UIScene extends Phaser.Scene {
+
+  selected: Unit[] = [];
+  dragbox!: Phaser.GameObjects.Graphics;
+  drawDragbox = false;
+
+  constructor() {
+    super('UIScene');
+  }
+
+  create() {
+    this.add.text(20, 20, "Left-click to select, drag left button to select multiple.\nRight click to move command.\nEnter to fullscreen.\nMouse wheel to scroll, drag mouse wheel to pan.");
+
+    this.dragbox = this.add.graphics();
+
+    this.input.keyboard.on('keydown-ENTER', function (this: UIScene) {
       if (!this.scale.isFullscreen) {
         this.scale.startFullscreen();
       } else {
         this.scale.stopFullscreen();
       }
-      
     }, this);
 
-    // this.input.on('pointerdown', function (this: Scene, pointer: Phaser.Input.Pointer) {
-    //   if (pointer.leftButtonDown()) {
-
-    //   }
-    // }, this);
-
-    this.input.on('pointermove', function (pointer: Phaser.Input.Pointer) {
+    this.input.on('pointermove', function (this: UIScene, pointer: Phaser.Input.Pointer) {
       if (pointer.leftButtonDown()) {
-        if (Phaser.Math.Distance.Squared(pointer.downX, pointer.downY, pointer.x, pointer.y) > 49) {
-          drawDragbox = true;
-          dragbox.clear();
-          dragbox.lineStyle(2, 0x00ff00, 1);
-          dragbox.strokeRect(pointer.downX, pointer.downY, pointer.x - pointer.downX, pointer.y - pointer.downY);
+        if (Phaser.Math.Distance.Squared(pointer.downX, pointer.downY, pointer.x, pointer.y) > 100) {
+          this.drawDragbox = true;
+          this.dragbox.clear();
+          this.dragbox.lineStyle(2, 0x00ff00, 1);
+          this.dragbox.strokeRect(pointer.downX, pointer.downY, pointer.x - pointer.downX, pointer.y - pointer.downY);
         }
       }
-
     }, this);
-
-    this.input.on('pointerup', function (this: Scene, pointer: Phaser.Input.Pointer, clickedObjects: Phaser.GameObjects.GameObject[]) {
-      if (pointer.leftButtonReleased()) {
-        sprites.forEach(sprite => {
-          sprite.setTint(0xffffff);
-        });
-        if (drawDragbox) {
-          this.selected = [];
-          drawDragbox = false;
-          dragbox.clear();
-
-          const boxX = Math.min(pointer.downX, pointer.x);
-          const boxY = Math.min(pointer.downY, pointer.y);
-          const width = Math.abs(pointer.getDistanceX());
-          const height = Math.abs(pointer.getDistanceY());
-          this.physics.overlapRect(boxX, boxY, width, height, true, false).forEach((body) => {
-            const obj = body.gameObject;
-            if (obj instanceof Unit) {
-              obj.setTint(0x00ff00);
-              this.selected.push(obj);
-            }
-          });
-        } else {
-          if (clickedObjects.length > 0) {
-            this.selected = [];
-            const unit = clickedObjects[0];
-            if (unit instanceof Unit) {
-              unit.setTint(0x00ff00);
-              this.selected.push(unit);
-            }
-          }
-        }
-
-      } else if (pointer.rightButtonReleased()) {
-        this.selected.forEach((unit) => {
-          this.physics.moveToObject(unit, pointer, unit.speed);
-          unit.movingToLoc = new Phaser.Math.Vector2(pointer);
-        });
-      }
-    }, this);
-
-  }
-
-  update() {
   }
 
 }
@@ -142,7 +159,7 @@ const config: Phaser.Types.Core.GameConfig = {
   physics: {
     default: 'arcade',
   },
-  scene: Scene
+  scene: [GameScene, UIScene]
 };
 
 new Phaser.Game(config);
